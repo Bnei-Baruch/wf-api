d89526535a8c5ee6b4433b71a8a7cd1e44f13657
amnonbb <amnonbb@gmail.com>
2022-12-16 11:57:14 +0200

    Clean up

--- api/routes.go
+++ api/routes.go
@@ -11,31 +11,4 @@ func SetupRoutes(router *gin.Engine) {
 	router.POST("/:root/:id/:key/:st", UpdateRecordState)
 
 	router.GET("/trim", GetTrimmed)
-
-	//router.GET("/ingest/find", GetIngestByKV)
-	//router.GET("/ingest/:id", GetIngestByID)
-	//router.PUT("/ingest/:id", PutIngest)
-	//router.POST("/ingest/:id/:st/:key", UpdateIngestState)
-	//router.POST("/ingest/:id/:key", PostIngestJSON)
-	//router.DELETE("/ingest/:id", RemoveIngest)
-	//
-	//router.GET("/trimmer/find", GetTrimmerByKV)
-	//router.GET("/trimmer/:id", GetTrimmerByID)
-	//router.PUT("/trimmer/:id", PutTrimmer)
-	//router.POST("/trimmer/:id/:st/:key", UpdateTrimmerState)
-	//router.POST("/trimmer/:id/:key", PostTrimmerJSON)
-	//router.DELETE("/trimmer/:id", RemoveTrimmer)
-	//router.GET("/trim", GetTrimmed)
-	//
-	//router.GET("/products/find", GetProductsByDF)
-	//router.GET("/products/:id", GetProductByID)
-	//router.PUT("/products/:id", PutProduct)
-	//router.POST("/products/:id/:st/:key", UpdateProductState)
-	//router.POST("/products/:id/:key", PostProductJSON)
-	//router.DELETE("/products/:id", RemoveProduct)
-	//
-	//router.GET("/kmedia/find", GetKmediaByKV)
-	//router.GET("/kmedia/:id", GetKmediaByID)
-	//router.PUT("/kmedia/:id", PutKmedia)
-	//router.DELETE("/kmedia/:id", RemoveKmedia)
 }
