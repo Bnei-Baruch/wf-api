6e7364979f950ecd2cd26325b27ae9287f241f45
amnonbb <amnonbb@gmail.com>
2023-12-06 14:21:01 +0200

    Fix syntax

--- models/jobs.go
+++ models/jobs.go
@@ -42,7 +42,7 @@ func FindJobsByUserID(values url.Values, t interface{}) (interface{}, error) {
 			continue
 		}
 		if k == "doers" {
-			where = append(where, fmt.Sprintf(`parent['%s'] ? '"%s"'`, k, v[0]))
+			where = append(where, fmt.Sprintf(`parent['%s'] ? '%s'`, k, v[0]))
 			continue
 		}
 		where = append(where, fmt.Sprintf(`"%s" = '%s'`, k, v[0]))
